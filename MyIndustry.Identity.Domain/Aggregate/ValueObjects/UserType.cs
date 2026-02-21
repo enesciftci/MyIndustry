ebc1d7c738dcef0d03b25c264f3d3f65eff7162d
enes.ciftci <enes.ciftci@hangikredi.com>
2026-02-21 22:39:42 +0300

    feat: add Admin user type for admin panel access control

--- MyIndustry.Identity.Domain/Aggregate/ValueObjects/UserType.cs
+++ MyIndustry.Identity.Domain/Aggregate/ValueObjects/UserType.cs
@@ -2,6 +2,8 @@ namespace MyIndustry.Identity.Domain.Aggregate.ValueObjects;
 
 public enum UserType : byte
 {
-    Purchaser = 1,
-    Seller = 2
+    User = 0,      // Normal kullanıcı
+    Purchaser = 1, // Alıcı (deprecated, User kullanılacak)
+    Seller = 2,    // Satıcı
+    Admin = 99     // Admin
 }
